e4b2e93d410f9ee67f770e9852f2dd9ea0a6be97
jxm <cjiangxumin@gmail.com>
2018-08-11 20:47:40 +0800

    ...

--- kube.sh
+++ kube.sh
@@ -2,41 +2,43 @@ create(){
 if [ ! -d $1 ];then
 	mkdir -vp  $1
 fi
-cat <<EOF >$1/$1_$2 
-FROM gcr.io/google_containers/$1:$2
+F_NAME=`echo $2| sed 's/:/_/'`
+cat <<EOF >$1/$F_NAME 
+FROM $3
 MAINTAINER Xumin Jiang <cjiangxumin@gmail.com>
 EOF
 }
 
 
-images=(kube-proxy-amd64:v1.11.0
-        kube-scheduler-amd64:v1.11.0
-        kube-controller-manager-amd64:v1.11.0
-        kube-apiserver-amd64:v1.11.0
-        etcd-amd64:3.2.18
-        coredns:1.1.3
-        pause-amd64:3.1
-        pause:3.1
-        kubernetes-dashboard-amd64:v1.8.3
-        k8s-dns-sidecar-amd64:1.14.8
-        k8s-dns-kube-dns-amd64:1.14.8
-        k8s-dns-dnsmasq-nanny-amd64:1.14.8 )
+# images=(kube-proxy-amd64:v1.11.0
+#         kube-scheduler-amd64:v1.11.0
+#         kube-controller-manager-amd64:v1.11.0
+#         kube-apiserver-amd64:v1.11.0
+#         etcd-amd64:3.2.18
+#         coredns:1.1.3
+#         pause-amd64:3.1
+#         pause:3.1
+#         kubernetes-dashboard-amd64:v1.8.3
+#         k8s-dns-sidecar-amd64:1.14.8
+#         k8s-dns-kube-dns-amd64:1.14.8
+#         k8s-dns-dnsmasq-nanny-amd64:1.14.8 )
+# 
 
-for imageName in ${images[@]} ; do
-	array=(${imageName//:/ })
+while read line
+do
+    #echo $line;
+    REPO_IMAGE=`echo $line | sed "s/\ //"`
+	BASE_NAME=`basename $REPO_IMAGE`
+	array=(${BASE_NAME//:/ })
 	IMG_NAME=${array[0]}	
-	IMG_VERSION=${array[1]}	
-	IMG_VERSION=${array[1]}	
-    echo ${IMG_NAME} : ${IMG_VERSION}
-    create ${IMG_NAME} ${IMG_VERSION}
-done
+	#IMG_VERSION=${array[1]}	
 
-git add .
-git commit -m "...."
-git pull origin master
+    #echo ${REPO_IMAGE}
+    create ${IMG_NAME} ${BASE_NAME}  ${REPO_IMAGE}
 
-#for imageName in ${images[@]} ; do
-#docker pull registry.cn-shenzhen.aliyuncs.com/duyj/$imageName
-#docker tag registry.cn-shenzhen.aliyuncs.com/duyj/$imageName k8s.gcr.io/$imageName
-#docker rmi registry.cn-shenzhen.aliyuncs.com/duyj/$imageName
-#done
+done < ./images.list
+
+# git add .
+# git commit -m "...."
+# git pull origin master
+
---
+++ kube_pull.sh
@@ -0,0 +1,58 @@
+# images=(kube-proxy-amd64:v1.11.0
+#         kube-scheduler-amd64:v1.11.0
+#         kube-controller-manager-amd64:v1.11.0
+#         kube-apiserver-amd64:v1.11.0
+#         etcd-amd64:3.2.18
+#         coredns:1.1.3
+#         pause-amd64:3.1
+#         pause:3.1
+#         kubernetes-dashboard-amd64:v1.8.3
+#         k8s-dns-sidecar-amd64:1.14.8
+#         k8s-dns-kube-dns-amd64:1.14.8
+#         k8s-dns-dnsmasq-nanny-amd64:1.14.8 )
+
+REGISTRY_ALIYUNCS=registry.cn-hangzhou.aliyuncs.com
+NAME_SPACE=k8s-gcr-io-jxm
+
+pull_and_rename()
+{
+	echo "docker pull ${REGISTRY_ALIYUNCS}/${NAME_SPACE}/$1"
+	#docker pull ${REGISTRY_ALIYUNCS}/${NAME_SPACE}/$1
+
+	echo "docker tag  ${REGISTRY_ALIYUNCS}/${NAME_SPACE}/$1 $2"
+	#docker tag  ${REGISTRY_ALIYUNCS}/${NAME_SPACE}/$1 $2
+
+	echo "docker rmi  ${REGISTRY_ALIYUNCS}/${NAME_SPACE}/$1"
+	#docker rmi  ${REGISTRY_ALIYUNCS}/${NAME_SPACE}/$1
+}
+
+create(){
+if [ ! -d $1 ];then
+	mkdir -vp  $1
+fi
+cat <<EOF >$1/$1_$2 
+FROM gcr.io/google_containers/$1:$2
+MAINTAINER Xumin Jiang <cjiangxumin@gmail.com>
+EOF
+}
+
+while read line
+do
+    #echo $line;
+    REPO_IMAGE=`echo $line | sed "s/\ //"`
+    BASE_NAME=`basename $REPO_IMAGE`
+	echo $imageName
+	pull_and_rename $BASE_NAME $REPO_IMAGE 
+
+done < ./images.list
+
+# REGISTRY_ALIYUNCS=registry.cn-hangzhou.aliyuncs.com
+# NAME_SPACE=k8s-gcr-io-jxm
+# for imageName in ${images[@]} ; do
+#    pull_and_rename $imageName
+#done
+
+# git add .
+# git commit -m "...."
+# git pull origin master
+
